b1f941e291de5c36066b3505b56f7e723d00af44
Arto Kir <artavazd1980@gmail.com>
2019-04-05 17:18:31 +0900

    Setup start screept and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 
@@ -22,7 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// 
+//
 // app.use((req, res, nest) => {
 //   res.render('maintenance.hbs', {
 //     pageTitle: 'maintenance Page',
@@ -70,6 +72,6 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
